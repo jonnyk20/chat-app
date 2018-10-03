0ca9a6585310a2b2504a7bfbf8b3ce9ca64a957f
Jonny Kalambay <jonnyk20@gmail.com>
2018-10-03 08:17:12 -0700

    removing doc ready call

--- client/main.js
+++ client/main.js
@@ -1,22 +1,20 @@
 var socket = io();
 
-$('document').ready(function() {
-  $('form').on('submit', function(e) {
-    var text = $('#message').val();
-    socket.emit('message', text);
-    $('#message').val('');
-    e.preventDefault();
-    return false;
-  });
+$('form').on('submit', function(e) {
+  var text = $('#message').val();
+  socket.emit('message', text);
+  $('#message').val('');
+  e.preventDefault();
+  return false;
+});
 
-  socket.on('message', function(msg) {
-    if (!msg) {
-      return;
-    }
-    $('#history').append([
-      $('<p>', { class: 'message' }).text(msg),
-      $('<div>', { class: 'clear' })
-    ]);
-    $('#history').scrollTop($('#history')[0].scrollHeight);
-  });
+socket.on('message', function(msg) {
+  if (!msg) {
+    return;
+  }
+  $('#history').append([
+    $('<p>', { class: 'message' }).text(msg),
+    $('<div>', { class: 'clear' })
+  ]);
+  $('#history').scrollTop($('#history')[0].scrollHeight);
 });
